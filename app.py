f55e93a3a65f9c85738b3dcd8a5c9aff262a13a2
Steven <contact@stevenroberts.info>
2021-02-20 12:13:25 +0000

    feat: added create part of CRUD

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # references this file - app.py
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # /// is a relative path - //// is an absolute path
+# init the database
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr_(self):
+        return '<Task %r>' % self.id
 
 
 # setup index route
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    # render_template knows to look in the templates folder
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        # render_template knows to look in the templates folder
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == "__main__":
